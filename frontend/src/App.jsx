a71029e0ab7c7e4feb5bbbeb7dc10b793778620b
sathwik pentakoti <sathwikpentakoti@gmail.com>
2025-07-17 19:25:43 +0530

    WIP: your message here

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="min-h-screen bg-slate-100 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-indigo-600">
-        Tailwind + React + Vite 🎉
+        Tailwind + React + Vite + SAtwik 🎉
       </h1>
     </div>
   );
