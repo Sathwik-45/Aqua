243951e7f27c8e25ba5588776b6074135653072b
Sai Krishna Bathina <saikrishna.bathina999@gmail.com>
2025-07-17 21:40:30 +0530

    deleted unnessary files

--- frontend/src/components/GradientBackground.jsx
+++
@@ -1,7 +0,0 @@
-const GradientBackground = () => {
-  return (
-    <div className="absolute inset-0 z-0 bg-gradient-to-br from-[#0093E9] to-[#80D0C7]"></div>
-  );
-};
-
-export default GradientBackground;
